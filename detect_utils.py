2be72b645b43734194c70ecd6ca2c74383836663
Akila_Ayanthi <aayanthi@gmail.com>
2021-12-18 18:45:32 +0530

    bbox

--- detect_utils.py
+++ detect_utils.py
@@ -21,6 +21,7 @@ def predict(image, model, device, detection_threshold):
     # transform the image to tensor
     # image = transform(image).to(device)
     # image = image.unsqueeze(0) # add a batch dimension
+    print('prediction')
     outputs = model(image) # get the predictions on the image
     # print the results individually
     # print(f"BOXES: {outputs[0]['boxes']}")
