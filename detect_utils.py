853781090cacb980a623908478fd28bc885d8158
Akila_Ayanthi <aayanthi@gmail.com>
2021-12-19 22:24:15 +0530

    SSD added

--- detect_utils.py
+++ detect_utils.py
@@ -315,7 +315,7 @@ def extract_frames(path ,file_name, model, model_name, min_size, savename, gt, d
                         text_c = cbbox[t]
                         gt_actual+=1
                         # print(gt_actual)
-                        if ious_actual[h]>0.0:
+                        if ious_actual[h]>0.1:
                             print(ious_actual[h])
                             detections+=1
                             # print(detections)
