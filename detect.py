72d7f9ff632d96d9d453766331b189abacf4b456
Akila_Ayanthi <aayanthi@gmail.com>
2021-12-27 11:36:21 +0530

    success rate per view

--- detect.py
+++ detect.py
@@ -49,11 +49,17 @@ gt.append(np.load('/home/dissana8/LAB/data/LAB/cam3_coords__.npy', allow_pickle=
 gt.append(np.load('/home/dissana8/LAB/data/LAB/cam4_coords__.npy', allow_pickle=True))
 
 fig, a = plt.subplots(4, 1)
-success_rate = detect_utils.extract_frames(path, file_name, model, args['model'], args['min_size'], savename, gt, device)
+success_rate, cam1_success_rate, cam2_success_rate, cam3_success_rate, cam4_success_rate = detect_utils.extract_frames(path, file_name, model, args['model'], args['min_size'], savename, gt, device)
 
 f = open("success_rate.txt", "a")
-f.write("Success rate of "+args['model']+" : "+str(success_rate))
+f.write("Success rate of "+args['model']+" : "+str(success_rate)+"\n")
+f.write("Success rate of view 01" +": "+str(cam1_success_rate)+"\n")
+f.write("Success rate of view 02" +": "+str(cam2_success_rate)+"\n")
+f.write("Success rate of view 03" +": "+str(cam3_success_rate)+"\n")
+f.write("Success rate of view 04" +": "+str(cam4_success_rate)+"\n")
 f.write("\n")
+f.write("\n")
+
 f.close()
 
 
